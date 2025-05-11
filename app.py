421ecc457eb4fba08e740876f49c9d90556dc3ab
Kunal Kalkotwar <kunalkalkotwar21@gmail.com>
2025-05-11 22:31:43 +0530

    CI/CD Added

--- app.py
+++ app.py
@@ -35,8 +35,7 @@ def index():
             pH =float(request.form['pH'])
             sulphates =float(request.form['sulphates'])
             alcohol =float(request.form['alcohol'])
-       
-         
+                
             data = [fixed_acidity,volatile_acidity,citric_acid,residual_sugar,chlorides,free_sulfur_dioxide,total_sulfur_dioxide,density,pH,sulphates,alcohol]
             data = np.array(data).reshape(1, 11)
             
